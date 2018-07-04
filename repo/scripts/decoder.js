080fea56dbd9e5f5e096f0ce1a1a3bb6f73dfc6b
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-07-04 11:11:29 -0700

    Added variables for pseudo insts

--- repo/scripts/decoder.js
+++ repo/scripts/decoder.js
@@ -7,6 +7,8 @@ decoder = {
   INST_FORMAT : "%inst%",
   sudoRegs : ["zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"],
   useSudoRegs : true,
+  pseudoDecode : false,
+  nopAll : false,
 
   getRegString : function(i) {
     i = Math.round(i);
